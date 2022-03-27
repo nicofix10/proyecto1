36007bec221a5901c78bdb985544dc7dc313d8ef
nicofix10 <nicolas.guerra_m@mail.udp.cl>
2022-03-27 17:21:44 -0400

    origin

--- index.php
+++ index.php
@@ -4,24 +4,70 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
     <title>Hello, world!</title>
   </head>
-  <body>
-    <h1>Hello, world!</h1>
+  <body class="d-flex h-100 text-center text-white bg-dark">
+    
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header class="mb-auto">
+        <div>
+          <h3 class="float-md-start mb-0">Cover</h3>
+          <nav class="nav nav-masthead justify-content-center float-md-end">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="#">Features</a>
+            <a class="nav-link" href="#">Contact</a>
+          </nav>
+        </div>
+      </header>
+    <h1>Ingrese Sueldo por mes</h1>
+      <main class="px-3">
+      <form action="calculos.php" method="post" target="_blank">
 
-    <!-- Optional JavaScript; choose one of the two! -->
+      <p>Diciembre: <input type="number" name="sueldod">
+    <p>Enero: <input type="number" name="sueldoe">
+    <p>Febrero: <input type="number" name="sueldof">
+    <p>Marzo: <input type="number" name="sueldom">
+    <p>Abril: <input type="number" name="sueldoa">
+    <p>Mayo: <input type="number" name="sueldomay">
+    <p>Junio: <input type="number" name="sueldoj">
+    <p>Julio: <input type="number" name="sueldojul">
+    <p>Agosto: <input type="number" name="sueldoag">
+    <p>Septiembre: <input type="number" name="sueldosep">
+    <p>Octubre: <input type="number" name="sueldooc">
+    <p>Noviembre: <input type="number" name="sueldonov">
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    -->
-  </body>
+    <h1>Ingrese Boletas de Honorarios por mes</h1>
+
+    <p>Diciembre: <input type="number" name="boletad">
+    <p>Enero: <input type="number" name="boletae">
+    <p>Febrero: <input type="number" name="boletaf">
+    <p>Marzo: <input type="number" name="boletam">
+    <p>Abril: <input type="number" name="boletaa">
+    <p>Mayo: <input type="number" name="boletamay">
+    <p>Junio: <input type="number" name="boletaj">
+    <p>Julio: <input type="number" name="boletajul">
+    <p>Agosto: <input type="number" name="boletaag">
+    <p>Septiembre: <input type="number" name="boletasep">
+    <p>Octubre: <input type="number" name="boletaoc">
+    <p>Noviembre: <input type="number" name="boletanov">
+
+    
+     <input type="submit" value="Enviar datos"></p>
+
+</form>
+      </main>
+    
+      <footer class="mt-auto text-white-50">
+        <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+      </footer>
+    </div>
+    
+    
+        
+      
+    
+    </body>
 </html>
